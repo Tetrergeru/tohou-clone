7ba317745391d3aaa9fcf228d13b9bb787f5f4c2
Tetrergeru <Tetrergeru@users.noreply.github.com>
2022-03-21 19:19:10 +0000

    deploy: c80a5a0c1c81dd219b50bff40ffad04f1201996f

--- index-b723fb92bf8cf4b8.js
+++ index-b723fb92bf8cf4b8.js
@@ -321,7 +321,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-d0231e195eb1f6ff_bg.wasm', import.meta.url);
+        input = new URL('index-b723fb92bf8cf4b8_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
